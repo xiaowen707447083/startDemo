e4c1ae7757b1822426d9e88ad2b3efea92fcfed4
lanou008 <707447083@qq.com>
2015-08-20 11:26:21 +0800

    文件操作

--- 读写本地文件/读写本地文件/main.swift
+++ 读写本地文件/读写本地文件/main.swift
@@ -10,3 +10,30 @@ import Foundation
 
 println("Hello, World!")
 
+//打印根目录/在ios中是沙盒目录
+var nshome = NSHomeDirectory()
+println(nshome)
+
+//文档所在目录
+var documentDirectory = nshome.stringByAppendingPathComponent("Documents")
+println(documentDirectory)
+
+var fileManager:NSFileManager = NSFileManager()
+//创建一个目录 withIntermediateDirectories:是否覆盖
+NSFileManager.defaultManager().createDirectoryAtPath("\(nshome)/测试目录_1", withIntermediateDirectories: false, attributes: nil, error: nil)
+
+//文件路径
+var filePath = documentDirectory.stringByAppendingPathComponent("测试.txt")
+var str = "测试写入的数据"
+//atomically:参数意思是如果为YES则保证文件的写入原子性,就是说会先创建一个临时文件,直到文件内容写入成功再导入到目标文件里.如果为NO,则直接写入目标文件里.
+str.writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
+
+//读取文件
+var sResult = String(contentsOfFile: filePath, encoding: NSUTF8StringEncoding, error: nil)
+println(sResult! as String)
+
+
+//获取文件路径
+ var paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true) as NSArray
+println(paths)
+
