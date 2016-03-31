7cce035b1bb267c39b4aa2ebbc629ffa1058d736
陈啸文 <100394@shenba.com>
2016-03-31 13:41:37 +0800

    c

---
+++ 普通测试项目/普通测试项目/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  普通测试项目
+//
+//  Created by shenba on 16/3/31.
+//  Copyright © 2016年 xiaowen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
