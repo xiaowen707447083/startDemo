5aa6154d4c7419d2d5ae98b6aafddab55750039f
陈啸文 <100394@shenba.com>
2016-03-31 18:07:22 +0800

    1

---
+++ MyFrame/MyFrame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFrame
+//
+//  Created by shenba on 16/3/31.
+//  Copyright © 2016年 xiaowen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
