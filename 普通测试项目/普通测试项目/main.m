fd36aff8f27666a2b1722b4dcc63a48dd0f6002a
陈啸文 <100394@shenba.com>
2016-04-22 17:12:25 +0800

    我

--- 普通测试项目/普通测试项目/main.m
+++ 普通测试项目/普通测试项目/main.m
@@ -20,6 +20,20 @@ int main(int argc, const char * argv[]) {
  
     
     NSString *str = @"ddddd";
+    
+
+        int a = 0x1234;
+        char b =  *(char *)&a;  //通过将int强制类型转换成char单字节，通过判断起始存储位置。即等于 取b等于a的低地址部分
+        if( b == 0x12)
+        {
+            NSLog(@"大端");
+        }else{
+            NSLog(@"小端");
+        }
+    
+
 
     return 0;
 }
+
+
